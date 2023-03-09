36cc07324a4e153253a7df3f1386dc876038e374
Brontozyabr <regisrohelek@gmail.com>
2023-03-10 00:34:58 +0100

    start

--- HelloGiItWorld.java
+++ HelloGiItWorld.java
@@ -1,5 +1,6 @@
 public class HelloGiItWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("goodbye my love goodbye");
     }
 }
